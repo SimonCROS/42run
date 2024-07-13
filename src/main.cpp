176a2680649ccc0ee4fc57d541bffee63fa5bc0b
Simon CROS <simon.dc.cros@gmail.com>
2024-07-13 22:26:53 +0200

    Add basic texture for uv visualization

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 
 #include "42runConfig.h"
 
+#define STBI_FAILURE_USERMSG
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
@@ -15,27 +16,33 @@ const GLuint WIDTH = 800, HEIGHT = 600;
 const char *vertexShaderSource =
     "#version 410\n"
     "\n"
-    "layout (location = 0) in vec3 i_pos;\n"
-    // "layout (location = 1) in vec3 i_color;\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "layout (location = 2) in vec2 aTexCoord;\n"
     "\n"
-    "layout (location = 0) out vec3 color;\n"
+    "layout (location = 0) out vec3 Color;\n"
+    "layout (location = 1) out vec2 TexCoord;\n"
     "\n"
     "void main()\n"
     "{\n"
-    "    gl_Position = vec4(i_pos, 1.0);\n"
-    "    color = i_pos;\n"
+    "    gl_Position = vec4(aPos, 1.0);\n"
+    "    Color = aColor;\n"
+    "    TexCoord = aTexCoord;\n"
     "}\0";
 
 // TODO Read from file
 const char *fragmentShaderSource =
     "#version 410\n"
     "\n"
-    "layout (location = 0) in vec3 color;\n"
+    "layout (location = 0) in vec3 Color;\n"
+    "layout (location = 1) in vec2 TexCoord;\n"
     "\n"
-    "layout (location = 0) out vec4 o_color;\n"
+    "layout (location = 0) out vec4 FragColor;\n"
+    "\n"
+    "uniform sampler2D Texture;"
     "\n"
     "void main() {\n"
-    "    o_color = vec4(color, 1.0);\n"
+    "    FragColor = texture(Texture, TexCoord) * vec4(Color, 1.0);\n"
     "}\0";
 
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
@@ -44,7 +51,7 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
         glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
-void error_callback(int error, const char* description)
+void error_callback(int error, const char *description)
 {
     fprintf(stderr, "Error: %s\n", description);
 }
@@ -68,27 +75,24 @@ int main(void)
 
     glfwSetKeyCallback(window, key_callback);
 
+    stbi_set_flip_vertically_on_load(true);
+
     int version = gladLoadGL(glfwGetProcAddress);
     std::cout << "42run " << PROJECT_VERSION_MAJOR << "." << PROJECT_VERSION_MINOR << std::endl;
     std::cout << "OpenGL " << GLAD_VERSION_MAJOR(version) << "." << GLAD_VERSION_MINOR(version) << std::endl;
 
     float vertices[] = {
-        0.5f, 0.5f, 0.0f,   // top right
-        0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f, // bottom left
-        -0.5f, 0.5f, 0.0f   // top left
+        // positions      // colors         // texture coords
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // top right
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // top left
     };
 
     unsigned int indices[] = {
         // note that we start from 0!
         0, 1, 3, // first triangle
         1, 2, 3  // second triangle
-    };
-
-    float texCoords[] = {
-        0.0f, 0.0f, // lower-left corner
-        1.0f, 0.0f, // lower-right corner
-        0.5f, 1.0f  // top-center corner
     };
 
     //! Generate buffers
@@ -111,8 +115,14 @@ int main(void)
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
         glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+        glEnableVertexAttribArray(2);
     }
 
     //! Compile vertex shader
@@ -176,6 +186,34 @@ int main(void)
         }
     }
 
+    //! Create texture
+    unsigned int texture;
+    {
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        int width, height, nrChannels;
+        stbi_uc *data = stbi_load("../assets/textures/uvs.png", &width, &height, &nrChannels, 0);
+
+        if (data == nullptr)
+        {
+            std::cerr << "ERROR::TEXTURE::LOADING_FAILED\n"
+                      << stbi_failure_reason() << std::endl;
+            stbi_image_free(data);
+            return 1;
+        }
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        stbi_image_free(data);
+    }
+
     glUseProgram(shaderProgram);
     while (!glfwWindowShouldClose(window))
     {
@@ -187,6 +225,7 @@ int main(void)
         // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
         glUseProgram(shaderProgram);
+        glBindTexture(GL_TEXTURE_2D, texture);
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
