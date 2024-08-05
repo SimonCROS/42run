ad7a59c2144528255fcc8a7211b1d8aa2f16bfa3
Simon Cros <simon.dc.cros@gmail.com>
2024-08-05 23:07:51 +0200

    Install glfw if not found

--- cmake/SetupExternalLibraries.cmake
+++ cmake/SetupExternalLibraries.cmake
@@ -15,4 +15,13 @@ add_subdirectory(${EXTERNAL_LIBRARIES_DIR}/tinygltf EXCLUDE_FROM_ALL)
 # Find OpenGL and glfw
 # ---------------------------------------------------------------------------------
 find_package(OpenGL 4.1 REQUIRED)
-find_package(glfw3 3.3 REQUIRED)
+
+find_package(glfw3 3.3 QUIET)
+if(NOT glfw_FOUND)
+    include(FetchContent)
+    FetchContent_Declare(glfw GIT_REPOSITORY https://github.com/glfw/glfw.git GIT_TAG master)
+    set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE)
+    set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+    set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+    FetchContent_MakeAvailable(glfw)
+endif()
